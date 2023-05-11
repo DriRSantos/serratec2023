7eb01fcf8c172e90c07860afde8e5129d2b0b239
DriRSantos <drirsantos@outlook.com>
2023-05-10 22:14:36 -0300

    aula 2, creating entities on sts

---
+++ Desenvolvimento-API-Restful-2023/ws-api-rest/biblioteca/src/main/java/com/residencia/biblioteca/BibliotecaApplication.java
@@ -0,0 +1,14 @@
+package com.residencia.biblioteca;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+// Spring annotation
+@SpringBootApplication
+public class BibliotecaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaApplication.class, args);
+	}
+
+}
